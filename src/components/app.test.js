07d8593fafc9201c2adac09a2086fca492261ec8
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-09-07 15:10:17 -0700

    started unit testing but getting ReferenceError: localStorage is not defined

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -3,14 +3,13 @@ import { shallow, mount, render } from 'enzyme';
 import App from './app';
 
 
-describe('Test-Run', () => {
-    it('should be able to run tests', () => {
-        expect(1 + 2).toEqual(3);
-    });
-});
 
 describe('<App />',()=>{
-     it('Renders without crashing', () => {
+   it('should be able to run tests', () => {
+      expect(1 + 2).toEqual(3);
+   });
+
+   it('Renders without crashing', () => {
        shallow(<App />);
    });
 })
