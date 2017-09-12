458083b8a4f265712fcb907147036c4e30a125eb
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-09-12 14:11:56 -0700

    added more tests that are currently failing

--- src/components/bloom.test.js
+++ src/components/bloom.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {shallow,mount} from 'enzyme';
-import App from './app';
+import Bloom from './bloom';
 
 //NOTE: add react-scripts test --env=jsdom to test in package.json instead of jest to reproduce referror localStorage undefined
 
 
-describe('<App />', () => {
+describe('<Bloom />', () => {
     it('Renders without crashing', () => {
-        shallow(<App />);
+        shallow(<Bloom />);
     });
 });
--- src/components/home.test.js
+++ src/components/home.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {shallow,mount} from 'enzyme';
-import App from './app';
+import Home from './home';
 
 //NOTE: add react-scripts test --env=jsdom to test in package.json instead of jest to reproduce referror localStorage undefined
 
 
-describe('<App />', () => {
+describe('<Home />', () => {
     it('Renders without crashing', () => {
-        shallow(<App />);
+        shallow(<Home />);
     });
 });
