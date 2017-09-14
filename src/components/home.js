9428444273e95f59e652b8e95960b7878e39a01f
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-09-14 15:53:27 -0700

    removed extra blank image on home screen

--- src/components/home.js
+++ src/components/home.js
@@ -116,13 +116,11 @@ class Home extends React.Component {
         			<div className="about-card-landings-container-1">
 
         					<div className="about-card-landing">
-        							<img className="about-icon-landing" role="presentation" src="assets/images/review.png" />
         							<h3>Add Customized Watering Zones</h3>
         							<hr className="short-line cards-line" />
         							<p> Bloom lets you organize every zone in your yard by assigning it a unique name of your choice.</p>
         					</div>
         					<div className="about-card-landing">
-        							<img className="about-icon-landing" role="presentation" src="assets/images/filter.png" />
         							<h3>Check Current Weather</h3>
         							<hr className="short-line cards-line" />
         							<p>Bloom provides current weather forecasts from a highly trusted weather service to help in generating an optimized watering plan for your yard.</p>
@@ -132,13 +130,11 @@ class Home extends React.Component {
 
         			<div className="about-card-landings-container-2">
         					<div className="about-card-landing">
-        						<img className="about-icon-landing" role="presentation" src="assets/images/local.png" />
         						<h3>Project Your Water Usage</h3>
         						<hr className="short-line cards-line" />
         						<p> Project the right watering schedule for your lawn, so you can put an end to over watering.</p>
         					</div>
         					<div className="about-card-landing">
-        						<img className="about-icon-landing" role="presentation" src="assets/images/local.png" />
         						<h3>Save Projections</h3>
         						<hr className="short-line cards-line" />
         						<p>Maintain a list of historical projections to get a better understanding of your water use through time.</p>
